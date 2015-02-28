d9b99eb3023581dcd699327928a207bc07a15e6c
Asim <asim@chuhnk.me>
2015-02-28 00:16:20 +0000

    add hash tag and url parsing

--- html/malten.js
+++ html/malten.js
@@ -1,3 +1,29 @@
+String.prototype.parseURL = function() {
+	return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function(url) {
+		var pretty = url.replace(/^http(s)?:\/\/(www\.)?/, '');
+		return pretty.link(url);
+	});
+};
+String.prototype.parseUsername = function() {
+	return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
+		var username = u.replace("@","");
+		return u.link("http://twitter.com/"+username);
+	});
+};
+String.prototype.parseHashTag = function() {
+	return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
+		//var tag = t.replace("#","%23")
+		return t.link(window.location+t);
+	});
+};
+
+function escapeHTML(str) {
+    var div = document.createElement('div');
+    div.style.display = 'none';
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+};
+
 function makeUL(array) {
         var list = document.createElement('ul');
 
@@ -6,8 +32,8 @@ function makeUL(array) {
         for(i = 0; i < array.length; i++) {
                 var item = document.createElement('li');
                 var div = document.createElement('div');
-		var text = document.createTextNode(array[i].Text);
-		div.appendChild(text);
+		var html = escapeHTML(array[i].Text);
+		div.innerHTML = html.parseURL().parseHashTag();
                 item.appendChild(div);
                 list.appendChild(item);
         }
