cd461df90f7d4a0cab890087f31867281f4fee69
asim <asim@aslam.me>
2023-04-10 17:19:53 +0100

    .

--- html/malten.js
+++ html/malten.js
@@ -57,39 +57,10 @@ function timeAgo() {
 
 function parseDate(tdate) {
     var system_date = new Date(tdate / 1e6);
-    var user_date = new Date();
     if (K.ie) {
         system_date = Date.parse(tdate.replace(/( \+)/, ' UTC$1'))
     }
-    var diff = Math.floor((user_date - system_date) / 1000);
-    if (diff < 0) {
-        return "0s";
-    }
-    if (diff < 60) {
-        return diff + "s";
-    }
-    if (diff <= 90) {
-        return "1m";
-    }
-    if (diff <= 3540) {
-        return Math.round(diff / 60) + "m";
-    }
-    if (diff <= 5400) {
-        return "1h";
-    }
-    if (diff <= 86400) {
-        return Math.round(diff / 3600) + "h";
-    }
-    if (diff <= 129600) {
-        return "1d";
-    }
-    if (diff < 604800) {
-        return Math.round(diff / 86400) + "d";
-    }
-    if (diff <= 777600) {
-        return "1w";
-    }
-    return "on " + system_date;
+    return system_date.toLocaleTimeString();;
 };
 
 // from http://widgets.twimg.com/j/1/widget.js
@@ -388,7 +359,6 @@ function loadMessages() {
             if (data != undefined && data.length > 0) {
                 displayMessages(data, 1);
                 clipMessages();
-                updateTimestamps();
             }
         })
         .fail(function(err) {
@@ -423,15 +393,6 @@ function pollMessages() {
     }, 5000);
 }
 
-function pollTimestamps() {
-    updateTimestamps();
-
-
-    setTimeout(function() {
-        pollTimestamps();
-    }, 10000);
-};
-
 
 function postMessage() {
     var form = document.getElementById('form');
@@ -462,7 +423,6 @@ function processEvent(stream, event) {
         events.push(ev);
         displayMessages(events, 1);
         clipMessages();
-        updateTimestamps();
         return;
     }
 
@@ -541,14 +501,6 @@ function tagText(text) {
         }
     }
     return parts.join(" ");
-};
-
-function updateTimestamps() {
-    var divs = document.getElementsByClassName('time');
-    for (i = 0; i < divs.length; i++) {
-        var time = divs[i].getAttribute('data-time');
-        divs[i].innerHTML = parseDate(time);
-    };
 };
 
 function shareListener() {
